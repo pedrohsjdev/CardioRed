eddb16d2e21cdfedf5eaca1ad495b44e527e298b
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-05 13:01:55 -0300

    New entity Medico

---
+++ backend/cardio/src/main/java/com/cardiored/cardio/domain/Medico.java
@@ -0,0 +1,37 @@
+package com.cardiored.cardio.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SuperBuilder
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 11, nullable = false)
+    private String crm;
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DoctorType doctorType;
+    
+    //@OneToOne
+    //@JoinColumn(name="userId", nullable = false)
+    //private User user;
+}
