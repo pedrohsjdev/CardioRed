dfed60dc9387918310c89413042f545c8410f098
Pedro Henrique <coc.cr.ph@gmail.com>
2022-05-04 23:33:29 -0300

    created spring project 2.6.6v

---
+++ backend/cardio/src/main/java/com/cardiored/cardio/CardioApplication.java
@@ -0,0 +1,13 @@
+package com.cardiored.cardio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardioApplication.class, args);
+    }
+
+}
