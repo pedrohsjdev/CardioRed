d10bd6b529a33594afc97305a0cb487b88970387
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-06 12:24:23 -0300

    Annotation @Repository added

--- backend/cardio/src/main/java/com/cardiored/cardio/repository/DocenteRepository.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/repository/DocenteRepository.java
@@ -3,7 +3,9 @@ package com.cardiored.cardio.repository;
 import com.cardiored.cardio.domain.Docente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocenteRepository extends JpaRepository<Docente, Integer> {
     
 }
--- backend/cardio/src/main/java/com/cardiored/cardio/repository/MedicoRepository.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/repository/MedicoRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.cardiored.cardio.domain.Medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, Integer> {
     
     List<Medico> findByName(String name);
