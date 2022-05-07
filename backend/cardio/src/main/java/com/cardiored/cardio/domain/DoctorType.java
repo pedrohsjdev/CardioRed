537faf6ec6cda34ee399093c43d4500da536e89b
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-07 00:14:10 -0300

    @JsonValue annotation to format doctorType in JSON

--- backend/cardio/src/main/java/com/cardiored/cardio/domain/DoctorType.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/domain/DoctorType.java
@@ -1,10 +1,13 @@
 package com.cardiored.cardio.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum DoctorType {
     MEDICO("Médico"),
     RESIDENTE("Residente"),
     DOCENTE("Docente");
 
+    @JsonValue
     private final String type;
 
     DoctorType(String type) {
