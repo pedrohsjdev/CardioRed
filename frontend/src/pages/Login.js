002a1246aca1aa23cae8d92a863edb73bd647808
Pedro Henrique <coc.cr.ph@gmail.com>
2022-05-11 23:38:03 -0300

    Bootstrap added to the project

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginContainer from "../components/LoginContainer";
+import LoginContainer from "../components/Login/LoginContainer";
 
 const Login = () => {
   return <LoginContainer />;
