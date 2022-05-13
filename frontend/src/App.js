83c8049a822ba90cd0932f77b070fb60f13e3055
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-13 13:16:45 -0300

    Pacientes route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Medicos from "./pages/Medicos";
+import Pacientes from "./pages/Pacientes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={ <Home/> }/>
-        <Route path="/home/medicos" element={<Medicos/>}/>
-      </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/home/medicos" element={<Medicos />} />
+                <Route path="/home/pacientes" element={<Pacientes />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
