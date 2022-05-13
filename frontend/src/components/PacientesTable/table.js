eaa9e3adf6f342bee44c3845339978aea0bd6746
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-13 13:17:04 -0300

    PacientesTable component created

---
+++ frontend/src/components/PacientesTable/table.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+const PacientesTable = () => {
+    //Temporary test data
+    const pacientes = [
+        {
+            id: 1,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 2,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 3,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 4,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 5,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 6,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 7,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 8,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 9,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+        {
+            id: 10,
+            cpf: "521.560.533-59",
+            nome: "Emily Aline Alícia Castro",
+            sexo: "Feminino",
+            cor: "Branco",
+            dataNascimento: "09/01/1999",
+        },
+    ];
+
+    return (
+        <>
+            <table className="table table-striped table-hover mt-3">
+                <thead>
+                    <tr>
+                        <th scope="col">CPF</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Sexo</th>
+                        <th scope="col">Cor</th>
+                        <th scope="col">Data de Nascimento</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pacientes.map((paciente) => (
+                        <tr key={paciente.id}>
+                            <td>{paciente.cpf}</td>
+                            <td>{paciente.nome}</td>
+                            <td>{paciente.sexo}</td>
+                            <td>{paciente.cor}</td>
+                            <td>{paciente.dataNascimento}</td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th scope="col">CPF</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Sexo</th>
+                        <th scope="col">Cor</th>
+                        <th scope="col">Data de Nascimento</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </>
+    );
+};
+
+export default PacientesTable;
