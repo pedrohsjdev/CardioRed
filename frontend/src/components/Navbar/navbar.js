f3d37c985ee8a2001603d0b11fba5d1d9a10e1df
Pedro Henrique <coc.cr.ph@gmail.com>
2022-05-12 21:33:11 -0300

    created home page and footer

--- frontend/src/components/Navbar/navbar.js
+++ frontend/src/components/Navbar/navbar.js
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <header>
             <nav class="navbar navbar-expand-lg navbar-light navbar-shadow">
-                <div class="container-fluid container-navbar">
+                <div class="container-fluid container-page">
                     <a class="navbar-brand" href="#" ><img src={Logo} /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
