c4115f53fe46e9cad063a137fc8a39fd1c1962b9
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-13 10:20:23 -0300

    Corrections on login page

--- frontend/src/components/Login/LoginContainer.js
+++ frontend/src/components/Login/LoginContainer.js
@@ -1,48 +1,53 @@
 import React from "react";
 import "./LoginContainer.css";
-import Logo  from '../../assets/logo.svg';
-import LoginImg  from '../../assets/img-login.svg';
+import Logo from "../../assets/logo.svg";
+import LoginImg from "../../assets/img-login.svg";
 
-function LoginContainer () {
+function LoginContainer() {
   return (
-      <div className="login-container">
-        <div className="login-content">
-          <div className="logo-img">
-            <img src={Logo}/>
-          </div>
+    <>
+      <div className="logo-content">
+        <div className="logo-img">
+          <img src={Logo} />
+        </div>
+      </div>
+      <div className="login-outer">
+        <div className="login-middle">
+          <div className="login-inner">
+            <div className="login-img">
+              <img src={LoginImg} />
+            </div>
 
-          <div className="login-img">
-          <img src={LoginImg}/>
-          </div>
+            <div className="login-forms">
+              <form action="" className="login" id="login-in">
+                <h1 className="login-title">Login</h1>
 
-          <div className="login-forms">
-            <form action="" className="login" id="login-in">
-              <h1 className="login-title">Login</h1>
+                <div className="login-box">
+                  <input
+                    type="text"
+                    placeholder="Usuário"
+                    className="login-input"
+                  />
+                </div>
 
-              <div className="login-box">
-                <input
-                  type="text"
-                  placeholder="Usuário"
-                  className="login-input"
-                />
-              </div>
+                <div className="login-box">
+                  <input
+                    type="password"
+                    placeholder="Senha"
+                    class="login-input"
+                  />
+                </div>
 
-              <div className="login-box">
-                <input
-                  type="password"
-                  placeholder="Senha"
-                  class="login-input"
-                />
-              </div>
-
-              <a href="#" className="login-button">
-                Entrar
-              </a>
-            </form>
+                <a href="#" className="login-button">
+                  Entrar
+                </a>
+              </form>
+            </div>
           </div>
         </div>
       </div>
+    </>
   );
-};
+}
 
 export default LoginContainer;
