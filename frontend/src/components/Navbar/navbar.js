ce881c5ee0e96f51d2d3e7fb747d747df511a944
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-14 20:13:26 -0300

    Navbar class attribute change to className

--- frontend/src/components/Navbar/navbar.js
+++ frontend/src/components/Navbar/navbar.js
@@ -1,36 +1,60 @@
 import React from "react";
 import "./style.css";
-import Logo from '../../assets/logo-navbar.svg';
+import Logo from "../../assets/logo-navbar.svg";
 
 function NavBar() {
     return (
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light navbar-shadow">
-                <div class="container-fluid container-page">
-                    <a class="navbar-brand" href="#" ><img src={Logo} /></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light navbar-shadow">
+                <div className="container-fluid container-page">
+                    <a className="navbar-brand" href="#">
+                        <img src={Logo} />
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="d-flex" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item ms-5">
-                                <a class="nav-link" href="#">Laudos</a>
+                    <div className="d-flex" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item ms-5">
+                                <a className="nav-link" href="#">
+                                    Laudos
+                                </a>
                             </li>
-                            <li class="nav-item ms-5">
-                                <a class="nav-link" href="#">Consultas</a>
+                            <li className="nav-item ms-5">
+                                <a className="nav-link" href="#">
+                                    Consultas
+                                </a>
                             </li>
-                            <li class="nav-item ms-5">
-                                <a class="nav-link" href="#">Pacientes</a>
+                            <li className="nav-item ms-5">
+                                <a
+                                    className="nav-link"
+                                    href="http://localhost:8080/pacientes"
+                                >
+                                    Pacientes
+                                </a>
                             </li>
-                            <li class="nav-item ms-5">
-                                <a class="nav-link" href="#">Médicos</a>
+                            <li className="nav-item ms-5">
+                                <a
+                                    className="nav-link"
+                                    href="http://localhost:8080/medicos"
+                                >
+                                    Médicos
+                                </a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
 
 export default NavBar;
