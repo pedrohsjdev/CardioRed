56825ad2cbbcf8d6368a344e9b1d8720c72f2bcd
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-14 23:51:28 -0300

    Added a Modal component on Pacientes page

--- frontend/src/pages/Pacientes.js
+++ frontend/src/pages/Pacientes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FormSearch from "../components/SearchBar/FormSearch";
 import CounterElements from "../components/TableCounter/CounterElements";
 import Pagination from "../components/Paginator/Pagination";
@@ -6,22 +6,37 @@ import NavBar from "../components/Navbar/navbar";
 import "./style.css";
 import Register from "../components/registerButton/register";
 import PacientesTable from "../components/PacientesTable/table";
+import Modal from "../components/Modal/Modal";
+import FormPaciente from "../components/FormPaciente/FormPaciente";
 
 const Pacientes = () => {
+    const [showModal, setShowModal] = useState(false);
+
     // Temporary test variables
-    let pagesNumber = [1, 2, 3, 4, 5];
+    const pagesNumber = [1, 2, 3, 4, 5];
 
     return (
         <>
             <NavBar />
+
+            <Modal
+                title="Cadastrar Paciente"
+                leftButtonText="Cancelar"
+                rightButtonText="Concluir"
+                show={showModal}
+                setShow={setShowModal}
+            >
+                <FormPaciente />
+            </Modal>
+
             <div className="page-container">
                 <h1>Listagem de Pacientes</h1>
                 <div className="d-flex justify-content-between">
-                    <Register />
+                    <Register setShowModal={setShowModal} />
                     <FormSearch criteria="CPF" />
                 </div>
 
-                <PacientesTable />
+                <PacientesTable setShowModal={setShowModal} />
                 <div className="d-flex justify-content-between">
                     <CounterElements
                         firstElement={"1"}
