f7e5a27af12fd0ea99f89008a7579028962ec58a
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-15 21:23:35 -0300

    Modals components were added to Paciente page

--- frontend/src/pages/Pacientes.js
+++ frontend/src/pages/Pacientes.js
@@ -6,37 +6,88 @@ import NavBar from "../components/Navbar/navbar";
 import "./style.css";
 import Register from "../components/registerButton/register";
 import PacientesTable from "../components/PacientesTable/table";
-import Modal from "../components/Modal/Modal";
+import ModalCreateAndUpdate from "../components/Modal/CreateAndUpdate/ModalCreateAndUpdate";
+import ModalView from "../components/Modal/View/ModalView";
 import FormPaciente from "../components/FormPaciente/FormPaciente";
+import ModalDelete from "../components/Modal/Delete/ModalDelete";
 
 const Pacientes = () => {
-    const [showModal, setShowModal] = useState(false);
+    // State variables for open and close the modals.
+    const [showModalCreateAndUpdate, setShowModalCreateAndUpdate] =
+        useState(false);
+    const [showModalView, setShowModalView] = useState(false);
+    const [showModalDelete, setShowModalDelete] = useState(false);
+
+    // State variable for store paciente data to be displayed on modal.
+    const [formData, setFormData] = useState({});
+
+    const [modalTitle, setModalTitle] = useState("");
 
     // Temporary test variables
     const pagesNumber = [1, 2, 3, 4, 5];
+
+    const openModalCreate = () => {
+        // Clean inputs
+        setFormData({});
+        setModalTitle("Cadastrar Paciente");
+        // Open modal
+        setShowModalCreateAndUpdate(true);
+    };
+
+    const openModalView = (data) => {
+        // Set clicked cell data to form inputs
+        setFormData(data);
+        // Open modal
+        setShowModalView(true);
+    };
+
+    const openModalUpdate = () => {
+        setModalTitle("Modificar Paciente");
+        // Open modal
+        setShowModalCreateAndUpdate(true);
+    };
+
+    const openModalDelete = () => {
+        // Open modal
+        setShowModalDelete(true);
+    };
 
     return (
         <>
             <NavBar />
 
-            <Modal
-                title="Cadastrar Paciente"
-                leftButtonText="Cancelar"
-                rightButtonText="Concluir"
-                show={showModal}
-                setShow={setShowModal}
+            <ModalCreateAndUpdate
+                show={showModalCreateAndUpdate}
+                setShow={setShowModalCreateAndUpdate}
+                title={modalTitle}
             >
-                <FormPaciente />
-            </Modal>
+                <FormPaciente disabled={false} data={formData} />
+            </ModalCreateAndUpdate>
+
+            <ModalView
+                show={showModalView}
+                setShow={setShowModalView}
+                openModalDelete={openModalDelete}
+                openModalUpdate={openModalUpdate}
+                title="Paciente"
+            >
+                <FormPaciente disabled={true} data={formData} />
+            </ModalView>
+
+            <ModalDelete
+                show={showModalDelete}
+                setShow={setShowModalDelete}
+                element="Paciente"
+            />
 
             <div className="page-container">
                 <h1>Listagem de Pacientes</h1>
                 <div className="d-flex justify-content-between">
-                    <Register setShowModal={setShowModal} />
+                    <Register openModalCreate={openModalCreate} />
                     <FormSearch criteria="CPF" />
                 </div>
 
-                <PacientesTable setShowModal={setShowModal} />
+                <PacientesTable openModalView={openModalView} />
                 <div className="d-flex justify-content-between">
                     <CounterElements
                         firstElement={"1"}
