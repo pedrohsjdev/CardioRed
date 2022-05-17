26d2ed7815e8fa6d61d5b4e7810380302a90b5f0
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-17 18:22:55 -0300

    Role class created

---
+++ backend/cardio/src/main/java/com/cardiored/cardio/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.cardiored.cardio.repository;
+
+import com.cardiored.cardio.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByName(String name);
+}
