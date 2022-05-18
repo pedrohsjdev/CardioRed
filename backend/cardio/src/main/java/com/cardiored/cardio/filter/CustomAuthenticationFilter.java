cd4fe5f47db608fe49656e4d85b8e574a1bd0df6
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-18 17:42:29 -0300

    Using util class on Authentication class

--- backend/cardio/src/main/java/com/cardiored/cardio/filter/CustomAuthenticationFilter.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/filter/CustomAuthenticationFilter.java
@@ -1,22 +1,16 @@
 package com.cardiored.cardio.filter;
 
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
+import com.cardiored.cardio.util.TokenCreator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.http.MediaType;
@@ -25,9 +19,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
 
@@ -40,8 +32,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                                 HttpServletResponse response) throws AuthenticationException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        log.info("Username is: {}", username);
-        log.info("Password is: {}", password);  
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authenticationToken);                                           
     }
@@ -51,22 +41,10 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authentication) throws ServletException, IOException {
-        User user = (User) authentication.getPrincipal();
 
-        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String access_token = TokenCreator.generateAccessToken(authentication, request.getRequestURL().toString());
 
-        String access_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-                .sign(algorithm);
-
-        String refresh_token = JWT.create()
-                .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
-                .withIssuer(request.getRequestURL().toString())
-                .sign(algorithm);
+        String refresh_token = TokenCreator.generateRefreshToken(authentication, request.getRequestURL().toString());
         
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", access_token);
