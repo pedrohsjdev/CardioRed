d03a674315fce8823c525e4ea07c26bd983097e9
DVastMasters <contatorafaeldutra@outlook.com>
2022-05-18 17:40:46 -0300

    Service logic removed from controller class

--- backend/cardio/src/main/java/com/cardiored/cardio/controller/UserController.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/controller/UserController.java
@@ -1,30 +1,19 @@
 package com.cardiored.cardio.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.cardiored.cardio.domain.Role;
 import com.cardiored.cardio.domain.User;
 import com.cardiored.cardio.request.user.UserPostDTO;
 import com.cardiored.cardio.request.user.UserPutDTO;
 import com.cardiored.cardio.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -79,42 +68,7 @@ public class UserController {
 
     @GetMapping(path = "/token/refresh")
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            try {
-                String refresh_token = authorizationHeader.substring("Bearer ".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-                JWTVerifier verifier = JWT.require(algorithm).build();
-                DecodedJWT decodeJWT = verifier.verify(refresh_token);
-                String username = decodeJWT.getSubject();
-                User user = userService.findByUsername(username); 
-
-                String access_token = JWT.create()
-                    .withSubject(user.getUsername())
-                    .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
-                    .withIssuer(request.getRequestURL().toString())
-                    .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
-                    .sign(algorithm);
-
-                Map<String, String> tokens = new HashMap<>();
-                tokens.put("access_token", access_token);
-                tokens.put("refresh_token", refresh_token);
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-
-            } catch (Exception e) {
-                response.setHeader("error", e.getMessage());
-                response.setStatus(HttpStatus.FORBIDDEN.value());
-                //response.sendError(HttpStatus.FORBIDDEN.value(), e.getMessage());
-                Map<String, String> tokens = new HashMap<>();
-                tokens.put("error_message", e.getMessage());
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-                
-            }
-        } else {
-            throw new RuntimeException  ("Refresh token is missing");
-        }
+        userService.refreshToken(request, response);
     }
     
 }
