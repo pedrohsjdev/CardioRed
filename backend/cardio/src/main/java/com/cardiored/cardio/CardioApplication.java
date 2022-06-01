ee5da9aa881cf9c7eb738ed7a4c53a9d8d1de5a3
Pedro Henrique <coc.cr.ph@gmail.com>
2022-05-31 22:53:09 -0300

    added user admin to application

--- backend/cardio/src/main/java/com/cardiored/cardio/CardioApplication.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/CardioApplication.java
@@ -1,10 +1,16 @@
 package com.cardiored.cardio;
 
+import com.cardiored.cardio.domain.Role;
+import com.cardiored.cardio.domain.User;
+import com.cardiored.cardio.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class CardioApplication {
@@ -17,5 +23,19 @@ public class CardioApplication {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_ADM"));
+            userService.saveRole(new Role(null, "ROLE_MEDICO"));
+            userService.saveRole(new Role(null, "ROLE_RESIDENTE"));
+            userService.saveRole(new Role(null, "ROLE_DOCENTE"));
+
+            userService.save(new User(null, "admin", "0000", new ArrayList<>()));
+            userService.addRoleToUser("admin", "ROLE_ADM");
+        };
+    }
+
 
 }
