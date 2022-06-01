c1834763108e4f83f0e3c8c7aa462b0914671aaf
DVastMasters <contatorafaeldutra@outlook.com>
2022-06-01 13:07:43 -0300

    Removing CommandLineRunner

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
-const EnvironmentPlugin = require("webpack");
+const { EnvironmentPlugin } = require("webpack");
 
 module.exports = {
     entry: "./src/index.js",
