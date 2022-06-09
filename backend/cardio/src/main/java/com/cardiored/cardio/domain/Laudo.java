46b70ed534e820de82541dd7576cb8fb0d9aede8
Iago Sampaio <iagobotelhoyt@gmail.com>
2022-06-08 22:41:26 -0300

    created laudo repository

--- backend/cardio/src/main/java/com/cardiored/cardio/domain/Laudo.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/domain/Laudo.java
@@ -1,15 +1,12 @@
 package com.cardiored.cardio.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.util.Date;
 
 @Data
