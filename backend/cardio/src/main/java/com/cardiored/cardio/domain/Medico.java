278a79c95b8637345a96d09b82c0e902156680b6
DVastMasters <contatorafaeldutra@outlook.com>
2022-06-13 10:13:13 -0300

    JsonIgnore added to Medico and Paciente

--- backend/cardio/src/main/java/com/cardiored/cardio/domain/Medico.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/domain/Medico.java
@@ -17,6 +17,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -36,19 +38,20 @@ public class Medico {
     @Column(length = 11)
     @NotNull
     private String crm;
-    
+
     @NotNull
     private String name;
 
     @Enumerated(EnumType.STRING)
     @NotNull
     private DoctorType doctorType;
-    
+
     @OneToOne
-    @JoinColumn(name="userId")
+    @JoinColumn(name = "userId")
     @NotNull
     private User user;
 
-    @OneToMany(cascade=CascadeType.ALL, mappedBy="medico")
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "medico")
     private List<Consulta> consultas;
 }
