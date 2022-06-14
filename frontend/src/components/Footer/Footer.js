b22b604a86b61ceeb87113d22e50db51af042411
DVastMasters <contatorafaeldutra@outlook.com>
2022-06-14 15:01:58 -0300

    Footer component refactored

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,21 +1,20 @@
 import React from "react";
-import "./style.css";
-import Heart from "../../assets/coracao.svg";
-import Github from "../../assets/github.svg";
+import "./Footer.css";
+import GitHub from "../../assets/git-hub.svg";
 
 const Footer = () => {
     return (
         <>
-            <footer className="mt-5">
+            <footer className="mt-5 footer-main-page">
                 <div className="container-fluid d-flex justify-content-between container-page footer-container">
                     <p className="footer-text">© 2022 CardioRed, Inc. Todos os direitos reservados.</p>
-                    <a href="https://github.com/PrinceHard/CardioRed" target="_blank" >
-                        <img src={Github} alt="logo_github" className="github" />
+                    <a href="https://github.com/PrinceHard/CardioRed" target="_blank">
+                        <img src={GitHub} alt="logo-github" className="github" />
                     </a>
                 </div>
             </footer>
         </>
-    )
-}
+    );
+};
 
 export default Footer;
