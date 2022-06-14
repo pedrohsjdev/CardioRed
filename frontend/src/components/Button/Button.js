91d6aefd6ea31e67b3e742ceeea427769048ea6f
DVastMasters <contatorafaeldutra@outlook.com>
2022-06-14 15:33:22 -0300

    Button component refactored

---
+++ frontend/src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({ value, action }) => {
+    return (
+        <>
+            <button className="btn btn-primary mt-5" onClick={() => action()}>
+                {value}
+            </button>
+        </>
+    );
+};
+
+export default Button;
