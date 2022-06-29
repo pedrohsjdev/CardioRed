46fdd88a284220f4bcec6815493c7b8877b35a0b
DVastMasters <contatorafaeldutra@outlook.com>
2022-06-29 13:14:09 -0300

    Added new authorization to ROLE_DOCENTE

--- backend/cardio/src/main/java/com/cardiored/cardio/security/SecurityConfig.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/users/**", "/medicos/**", "/residentes/**", "/docentes/**")
                 .hasAnyAuthority("ROLE_ADM");
+        http.authorizeRequests().antMatchers("/medicos/**").hasAnyAuthority("ROLE_DOCENTE");
         http.authorizeRequests().antMatchers("/pacientes/**").hasAnyAuthority("ROLE_ADM", "ROLE_MEDICO",
                 "ROLE_RESIDENTE", "ROLE_DOCENTE");
         http.authorizeRequests().anyRequest().authenticated();
