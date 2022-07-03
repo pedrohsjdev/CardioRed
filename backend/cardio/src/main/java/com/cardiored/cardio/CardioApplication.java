627a0127c2f553069df36fb48e4db44d3b462c36
DVastMasters <contatorafaeldutra@outlook.com>
2022-07-03 19:10:53 -0300

    Fixed Consulta backend classes

--- backend/cardio/src/main/java/com/cardiored/cardio/CardioApplication.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/CardioApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @SpringBootApplication
 public class CardioApplication {
 
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
         SpringApplication.run(CardioApplication.class, args);
     }
 
