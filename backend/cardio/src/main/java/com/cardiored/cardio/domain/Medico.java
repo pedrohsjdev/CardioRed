dd6b6ec22f15a20fb2e740dc105d54d36ccc0236
DVastMasters <contatorafaeldutra@outlook.com>
2022-07-03 19:08:46 -0300

    Medico replace error fixed

--- backend/cardio/src/main/java/com/cardiored/cardio/domain/Medico.java
+++ backend/cardio/src/main/java/com/cardiored/cardio/domain/Medico.java
@@ -1,8 +1,5 @@
 package com.cardiored.cardio.domain;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -13,18 +10,14 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -52,12 +45,4 @@ public class Medico {
     @JoinColumn(name = "userId")
     @NotNull
     private User user;
-
-    @JsonIgnore
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "medico")
-    private Set<Laudo> laudo;
-
-    @JsonIgnore
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "medico")
-    private List<Consulta> consultas;
 }
