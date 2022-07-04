7186431386f514dfc57ae2fd5796a620c0446bcb
DVastMasters <contatorafaeldutra@outlook.com>
2022-07-04 13:36:57 -0300

    Added icons to FormViewConsulta component

--- frontend/src/components/Form/FormConsulta/FormViewConsulta/FormViewConsulta.js
+++ frontend/src/components/Form/FormConsulta/FormViewConsulta/FormViewConsulta.js
@@ -3,58 +3,103 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Printer from "../../../../assets/print-icon.svg";
+import TaskChecked from "../../../../assets/task-checked.svg";
 import "./FormViewConsulta.css";
-import { generatePDF } from "../../../../services/Consulta/ConsultaService";
+import { generatePDF, updateConsulta } from "../../../../services/Consulta/ConsultaService";
+import { Confirm, Notify } from "notiflix";
 
-const FormViewConsulta = ({ consultaData }) => {
+const FormViewConsulta = ({ consultaData, setConsultaData, openModalUpdate, openModalDelete, setShow }) => {
+    const finishedConsulta = () => {
+        Confirm.show(
+            "Consulta realizada",
+            "Deseja mudar o status da consulta para 'Aguardando laudo'?",
+            "Sim",
+            "Não",
+            () => {
+                updateConsulta({ ...consultaData, status: "Aguardando laudo" }).then(() => {
+                    Notify.success("Status modificado.");
+                });
+                setConsultaData({ ...consultaData, status: "Aguardando laudo" });
+            },
+            () => {},
+            {
+                titleColor: "#b5303e",
+                width: "350px",
+                titleFontSize: "1.3rem",
+                messageFontSize: "1rem",
+                buttonsFontSize: "1rem",
+                okButtonBackground: "#b5303e",
+            }
+        );
+    };
+
+    const renderPrintIcon = () => {
+        if (consultaData.status === "Aguardando exame") {
+            return (
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        generatePDF(consultaData.paciente.name, consultaData.dateTime);
+                    }}
+                    className="btn-print-consulta"
+                >
+                    <img className="img-print-consulta" src={Printer} />
+                </button>
+            );
+        }
+    };
+    const renderChangeStatusIcon = () => {
+        if (consultaData.status === "Aguardando exame") {
+            return (
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        finishedConsulta();
+                    }}
+                    className="btn-print-consulta"
+                >
+                    <img className="img-print-consulta" src={TaskChecked} />
+                </button>
+            );
+        }
+    };
     return (
         <Form>
             <Form.Group as={Row} className="mb-3">
-                <Form.Label column sm={2}>
-                    Identificador*:
+                <Form.Label column sm={3}>
+                    Identificador:
                 </Form.Label>
-                <Col sm={3}>
-                    <Form.Control defaultValue={consultaData.id} disabled name="id" type="number" />
+                <Col sm={8}>
+                    <Form.Control defaultValue={consultaData.id} disabled type="number" />
                 </Col>
-                <Col className="print-container">
-                    <button
-                        onClick={(e) => {
-                            e.preventDefault();
-                            generatePDF(consultaData.paciente.name, consultaData.dateTime);
-                        }}
-                        className="btn-print-consulta"
-                    >
-                        <img className="img-print-consulta" src={Printer} />
-                    </button>
-                </Col>
+                <Col className="print-container">{renderPrintIcon()}</Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3">
-                <Form.Label column sm={2}>
-                    Paciente*:
+                <Form.Label column sm={3}>
+                    Paciente:
                 </Form.Label>
-                <Col sm={10}>
+                <Col sm={9}>
                     <Form.Control
                         defaultValue={consultaData.paciente ? consultaData.paciente.name : ""}
                         disabled
-                        name="paciente"
                         type="text"
                     />
                 </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3">
                 <Form.Label column sm={3}>
-                    Data e Hora*:
+                    Data e Hora:
                 </Form.Label>
                 <Col sm={9}>
-                    <Form.Control defaultValue={consultaData.dateTime} disabled name="dateTime" type="localDateTime" />
+                    <Form.Control defaultValue={consultaData.dateTime} disabled type="text" />
                 </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3">
-                <Form.Label column sm={2}>
-                    Exame*:
+                <Form.Label column sm={3}>
+                    Exame:
                 </Form.Label>
-                <Col sm={10}>
-                    <Form.Select defaultValue={consultaData.examType} disabled name="examType">
+                <Col sm={9}>
+                    <Form.Select defaultValue={consultaData.examType} disabled>
                         <option value="">Escolha uma opção</option>
                         <option value="Ecocardiograma">Ecocardiograma</option>
                         <option value="Eletrocardiograma">Eletrocardiograma</option>
@@ -64,20 +109,50 @@ const FormViewConsulta = ({ consultaData }) => {
                 </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3">
-                <Form.Label column sm={4}>
-                    Hipótese diagnóstica*:
+                <Form.Label column sm={3}>
+                    Hipótese diagnóstica:
                 </Form.Label>
-                <Col sm={8}>
+                <Col sm={9}>
                     <Form.Control
                         defaultValue={
                             consultaData.diagnosticAssumption.code + " - " + consultaData.diagnosticAssumption.name
                         }
+                        className="input-heigth-large"
                         disabled
-                        name="diagnosticAssumption"
                         type="text"
                     />
                 </Col>
             </Form.Group>
+            <Form.Group as={Row} className="mb-3">
+                <Form.Label column sm={3}>
+                    Status:
+                </Form.Label>
+                <Col sm={8}>
+                    <Form.Control value={consultaData.status} disabled type="text" />
+                </Col>
+                <Col className="print-container">{renderChangeStatusIcon()}</Col>
+            </Form.Group>
+            <div className="modal-footer d-flex justify-content-between">
+                <button
+                    type="button"
+                    className="btn btn-primary btn-modal btn-left"
+                    onClick={() => {
+                        openModalDelete();
+                    }}
+                >
+                    Remover
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-primary btn-modal"
+                    onClick={() => {
+                        setShow(false);
+                        openModalUpdate();
+                    }}
+                >
+                    Modificar
+                </button>
+            </div>
         </Form>
     );
 };
